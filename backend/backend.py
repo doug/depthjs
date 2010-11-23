22f5933b8f5fa48e9e93d9f085f4570dbdb0259b
Doug Fritz <doug@media.mit.edu>
2010-11-22 19:08:29 -0500

    forwarder working for zeromq and websocket

--- backend/backend.py
+++ backend/backend.py
@@ -4,7 +4,7 @@ from zmq.eventloop import ioloop
 import tornado.httpserver
 import tornado.web
 import tornado.websocket
-import tornado.options import define, options
+from tornado.options import define, options
 
 from random import random
 import json
@@ -15,99 +15,51 @@ import subprocess
 
 define("port", default=8000, help="port")
 define("source", default="localhost", help="data source host")
-define("event_port", default="14444", help="data event port")
-define("image_port", default="14445", help="data image port")
-define("depth_port", default="14446", help="data depth port")
+define("event_port", default=14444, type=int, help="data event port")
+define("image_port", default=14445, type=int, help="data image port")
+define("depth_port", default=14446, type=int, help="data depth port")
 
+context = zmq.Context()
 loop = ioloop.IOLoop.instance()
 
 # ZEROMQ
-class EventReader(object):
-  def __init__(self,chan,context,loop):
-    self.chan = chan
-    self.datasocket = context.socket(zmq.SUB)
-    self.datasocket.connect("%s:%i"%(DATA_ADDRESS,DATA_PORT+chan))
-    self.datasocket.setsockopt(zmq.SUBSCRIBE,'')
-    loop.add_handler(self.datasocket, self.read_data, zmq.POLLIN)
-  def read_data(self, sock, events):
-    if websockets.has_key(self.chan):
-      dp = data_pb2.DataPacket()
-      dp.ParseFromString(sock.recv())
-      data = downsample(dp.samples,DOWNSAMPLE)
-      jsonmsg = json.dumps(data)
-      for s in websockets[self.chan]:
-        s.write_message(jsonmsg)
-
-class ImageReader(object):
-  def __init__(self,chan,context,loop):
-    self.chan = chan
-    self.datasocket = context.socket(zmq.SUB)
-    self.datasocket.connect("%s:%i"%(DATA_ADDRESS,DATA_PORT+chan))
-    self.datasocket.setsockopt(zmq.SUBSCRIBE,'')
-    loop.add_handler(self.datasocket, self.read_data, zmq.POLLIN)
-  def read_data(self, sock, events):
-    if websockets.has_key(self.chan):
-      dp = data_pb2.DataPacket()
-      dp.ParseFromString(sock.recv())
-      data = downsample(dp.samples,DOWNSAMPLE)
-      jsonmsg = json.dumps(data)
-      for s in websockets[self.chan]:
-        s.write_message(jsonmsg)
-
 class Forwarder(object):
-  def __init__(self,chan,context,loop):
-    self.chan = chan
+  def __init__(self,source,source_port,socktype,loop):
+    self.socktype = socktype
     self.datasocket = context.socket(zmq.SUB)
-    self.datasocket.connect("%s:%i"%(DATA_ADDRESS,DATA_PORT+chan))
+    address = "tcp://%s:%i"%(source,source_port)
+    print(address)
+    self.datasocket.connect(address)
     self.datasocket.setsockopt(zmq.SUBSCRIBE,'')
     loop.add_handler(self.datasocket, self.read_data, zmq.POLLIN)
   def read_data(self, sock, events):
-    if websockets.has_key(self.chan):
-      dp = data_pb2.DataPacket()
-      dp.ParseFromString(sock.recv())
-      data = downsample(dp.samples,DOWNSAMPLE)
-      jsonmsg = json.dumps(data)
-      for s in websockets[self.chan]:
-        s.write_message(jsonmsg)
+    if websockets.has_key(self.socktype):
+      clients = websockets[self.socktype]
+      if clients.empty() == false:
+        data = sock.recv()
+        for s in clients:
+          s.write_message(data)
 
-
-
-context = zmq.Context()
-readers = {}
-for chan in range(NUM_CHANNELS):
-  dr = DataReader(chan,context,loop)
-  readers[chan] = dr
-
-
+Forwarder(options.source, options.event_port, "events", loop)
+Forwarder(options.source, options.image_port, "image", loop)
+Forwarder(options.source, options.depth_port, "depth", loop)
 
 # SOCKETS
 websockets = {}
 
-class ImageSocket(tornado.websocket.WebSocketHandler):
-  def open(self, *args, **kwargs):
-    print("open")
-  def on_message(self, message):
-    print(message)
-  def on_close(self):
-    print("close")
+def gen_socket(socktype):
+  class DataSocket(tornado.websocket.WebSocketHandler):
+    def open(self, *args, **kwargs):
+      websockets[socktype] = websockets.get(socktype,[])
+      websockets[socktype].append(self)
+      print("opened %s" % (socktype,))
+    def on_message(self, message):
+      print(message)
+    def on_close(self):
+      websockets[socktype].remove(self)
+      print("closed %s" % (socktype,))
+  return DataSocket
 
-class DepthSocket(tornado.websocket.WebSocketHandler):
-  def open(self, *args, **kwargs):
-    print("open")
-  def on_message(self, message):
-    print(message)
-  def on_close(self):
-    print("close")
-
-class EventSocket(tornado.websocket.WebSocketHandler):
-  def open(self, *args, **kwargs):
-    print("open")
-  def on_message(self, message):
-    print(message)
-  def on_close(self):
-    print("close")
-
-# watch files for events when modules are updated
 
 settings = {
   "static_path": os.path.join(os.path.dirname(__file__), "static"),
@@ -115,13 +67,13 @@ settings = {
 }
 
 application = tornado.web.Application([
-  (r"/events", EventSocket),
-  (r"/image", ImageSocket),
-  (r"/depth", DepthSocket),
+  (r"/events", gen_socket("events")),
+  (r"/image", gen_socket("image")),
+  (r"/depth", gen_socket("depth")),
 ], **settings)
 
 if __name__ == "__main__":
   http_server = tornado.httpserver.HTTPServer(application, io_loop=loop)
   http_server.listen(options.port)
-  print "starting localhost:"+options.port
+  print "starting %s:%i" % ("localhost", options.port)
   loop.start()
