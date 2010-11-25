9e77884209dc16e69abae92c986e16016c56d32e
Roy Shilkrot <roys@media.mit.edu>
2010-11-25 13:07:28 -0500

    code clean up

--- cv/ocv_freenect.cpp
+++ cv/ocv_freenect.cpp
@@ -3,24 +3,12 @@
  *  OpenCVTries1
  *
  *  Created by Roy Shilkrot on 11/19/10.
- *  Copyright 2010 MIT. All rights reserved.
  *
  */
 
 #define LIBUSB_DEBUG 5
 
 #include "ocv_freenect.h"
-
-
-//#if defined(__APPLE__)
-//#include <GLUT/glut.h>
-//#include <OpenGL/gl.h>
-//#include <OpenGL/glu.h>
-//#else
-//#include <GL/glut.h>
-//#include <GL/gl.h>
-//#include <GL/glu.h>
-//#endif
 
 #include <math.h>
 
@@ -45,31 +33,10 @@ int freenect_led;
 pthread_cond_t frame_cond = PTHREAD_COND_INITIALIZER;
 int got_frames = 0;
 
-void *ocv_threadfunc(void *arg)
-{
-	printf("opencv thread\n");
-
-	while (!die) {
-//		pthread_mutex_lock(&buf_mutex);
-
-		imshow("rgb", rgbMat);
-//		imshow("depth",depthMat);
-
-//		pthread_cond_signal(&frame_cond);
-//		pthread_mutex_unlock(&buf_mutex);
-
-		int c = waitKey(30);
-		if(c == 27) die = true;
-	}
-
-	pthread_exit(NULL);
-	return NULL;
-}
-
 uint16_t t_gamma[2048];
 freenect_context *f_ctx;
 
-void *freenect_threadfunc(void* arg) {
+void *freenect_threadfunc(void* arg) {	//all this thread does is to fetch events from freenect
 	cout << "freenect thread"<<endl;
 	while(!die && freenect_process_events(f_ctx) >= 0 ) {}
 	cout << "freenect die"<<endl;
@@ -99,6 +66,7 @@ void rgb_cb(freenect_device *dev, freenect_pixel *rgb, uint32_t timestamp)
 	pthread_mutex_unlock(&buf_mutex);
 }
 
+//functions defined in the other file...
 extern  Scalar refineSegments(const Mat& img,
 							  Mat& mask,
 							  Mat& dst,
@@ -107,6 +75,7 @@ extern  Scalar refineSegments(const Mat& img,
 extern void makePointsFromMask(Mat& maskm,vector<Point2f>& points, bool _add = false);
 extern void drawPoint(Mat& out,vector<Point2f>& points,Scalar color, Mat* maskm = NULL);
 
+//the ZMQ socket used to transfer data to the browser
 zmq::context_t context (1);
 zmq::socket_t socket (context, ZMQ_PUB);
 
@@ -131,6 +100,7 @@ void send_image(const Mat& img) {
 	s_send(socket, (const char*)_img.data);
 }
 
+//Calculating the laplacian of a 2D curve. Thanks Y.Gingold!
 Mat laplacian_mtx(int N, bool closed_poly) {
 	Mat A = Mat::zeros(N, N, CV_64FC1);
 	Mat d = Mat::zeros(N, 1, CV_64FC1);
@@ -162,15 +132,15 @@ Mat laplacian_mtx(int N, bool closed_poly) {
 
 void calc_laplacian(Mat& X, Mat& Xlap) {
 	static Mat lapX = laplacian_mtx(X.rows,false);
-	if(lapX.rows != X.rows) lapX = laplacian_mtx(X.rows,false);
+		//a feeble attempt to save up in memory allocation.. in 99.9% of the cases this if fires
+	if(lapX.rows != X.rows) lapX = laplacian_mtx(X.rows,false); 
 	
-	Mat _X;
+	Mat _X;	//handle non-64UC2 matrices
 	if (X.type() != CV_64FC2) {
 		X.convertTo(_X, CV_64FC2);
 	} else {
 		_X = X;
 	}
-
 	
 	vector<Mat> v; split(_X,v);
 	v[0] = v[0].t() * lapX.t();
@@ -184,11 +154,6 @@ void calc_laplacian(Mat& X, Mat& Xlap) {
 int main(int argc, char **argv)
 {
 	int res;
-
-//	cvtColor(rgbMat, rgbMat, CV_RGB2BGR);
-//	namedWindow("rgb");
-//	namedWindow("depth");
-
 
 	try {
 		socket.bind ("tcp://*:14444");
@@ -212,6 +177,7 @@ int main(int argc, char **argv)
 	g_argc = argc;
 	g_argv = argv;
 
+	//setup Freenect...
 	if (freenect_init(&f_ctx, NULL) < 0) {
 		printf("freenect_init() failed\n");
 		return 1;
@@ -234,7 +200,6 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-
 	freenect_set_tilt_degs(f_dev,freenect_angle);
 	freenect_set_led(f_dev,LED_RED);
 	freenect_set_depth_callback(f_dev, depth_cb);
@@ -245,15 +210,15 @@ int main(int argc, char **argv)
 	freenect_start_depth(f_dev);
 	freenect_start_rgb(f_dev);
 
+	//start the freenect thread to poll for events
 	res = pthread_create(&ocv_thread, NULL, freenect_threadfunc, NULL);
 	if (res) {
 		printf("pthread_create failed\n");
 		return 1;
 	}
 
-	Mat depthf; //(depthMat.size(),CV_32FC1,Scalar(0));
+	Mat depthf;
 
-//	CvBGStatModel* bg_model = 0;
 	Mat frameMat(rgbMat);
 	Mat out(frameMat.size(),CV_8UC1),
 		outC(frameMat.size(),CV_8UC3);
@@ -264,7 +229,6 @@ int main(int argc, char **argv)
 	vector<uchar> statusv;
 	vector<float> errv;
 	Rect cursor(frameMat.cols/2,frameMat.rows/2,10,10);
-//	int nmfr = 0; //non-motion frames counter
 	bool update_bg_model = true;
 	int fr = 1;
 	int register_ctr = 0;
@@ -275,7 +239,6 @@ int main(int argc, char **argv)
 	Point2i midBlob(-1,-1);
 	Point2i lastMove(-1,-1);
 	
-//	int hc_ctr = 0;
 	int hcr_ctr = -1;
 	vector<int> hc_stack(20); int hc_stack_ptr = 0;
 	
@@ -289,11 +252,13 @@ int main(int argc, char **argv)
 			_tmp.setTo(Scalar(2048), depthMat > 600.0);		//cut off at 600 to create a "box" where the user interacts
 			_tmp.convertTo(depthf, CV_8UC1, 255.0/1648.0);	//values are 0-2048 (11bit), account for -400 = 1648
 		}
-//		{
+		
+//		{ //saving the frames to files for debug
 //			stringstream ss; ss << "depth_"<<fr<<".png";
 //			imwrite(ss.str(), depthf);
 //		}
-		//Logarithm interpolcation
+		
+		//Logarithm interpolation - try it!, It should be more "sensitive" for closer depths
 //		{
 //			Mat tmp,tmp1;
 //			depthMat.convertTo(tmp, CV_32FC1);
@@ -302,75 +267,13 @@ int main(int argc, char **argv)
 //		}
 //		imshow("depth",depthf);
 
-		/*
-		//Mix grayscale and depth for bg-fg model
-//		Mat rgbAndDepth,gray; cvtColor(rgbMat, gray, CV_BGR2GRAY);
-//		equalizeHist(gray, gray);
-//		vector<Mat> cs; //split(rgbAndDepth,cs);
-//		cs.push_back(depthf);
-//		cs.push_back(gray);
-//		cs.push_back(Mat::zeros(depthf.size(), CV_8UC1));
-//		merge(cs,rgbAndDepth);
-
-//		imshow("rgb",rgbAndDepth);
-
-//		IplImage rgbIplI = (IplImage)depthf;
-//		IplImage rgbIplI = (IplImage)rgbAndDepth;
-
-//		if(!bg_model)
-//        {
-//			CvGaussBGStatModelParams params;
-
-//#define CV_BGFG_MOG_BACKGROUND_THRESHOLD     0.7     /* threshold sum of weights for background test /
-//#define CV_BGFG_MOG_STD_THRESHOLD            2.5     /* lambda=2.5 is 99% /
-//#define CV_BGFG_MOG_WINDOW_SIZE              200     /* Learning rate; alpha = 1/CV_GBG_WINDOW_SIZE /
-//#define CV_BGFG_MOG_NGAUSSIANS               5       /* = K = number of Gaussians in mixture /
-//#define CV_BGFG_MOG_WEIGHT_INIT              0.05
-//#define CV_BGFG_MOG_SIGMA_INIT               30
-//#define CV_BGFG_MOG_MINAREA                  15.f
-
-//			params.win_size      = 100; //CV_BGFG_MOG_WINDOW_SIZE;
-//			params.bg_threshold  = 0.7;
-//
-//			params.std_threshold = CV_BGFG_MOG_STD_THRESHOLD;
-//			params.weight_init   = CV_BGFG_MOG_WEIGHT_INIT;
-//
-//			params.variance_init = CV_BGFG_MOG_SIGMA_INIT*CV_BGFG_MOG_SIGMA_INIT;
-//			params.minArea       = CV_BGFG_MOG_MINAREA;
-//			params.n_gauss       = 5;
-//
-//            //create BG model
-////            bg_model = cvCreateGaussianBGModel( &rgbIplI , &params);
-//
-//            //bg_model = cvCreateFGDStatModel( &rgbIplI );
-//            continue;
-//        }
-
-		//Background-forground model
-//        double t = (double)cvGetTickCount();
-//        cvUpdateBGStatModel( &rgbIplI, bg_model, update_bg_model ? -1 : 0 );
-//        t = (double)cvGetTickCount() - t;
-
-		if(fr == 50) update_bg_model = true;
-
-		//        printf( "%d. %.1f\n", fr, t/(cvGetTickFrequency()*1000.) );
-		//        cvShowImage("BG", bg_model->background);
-		//        cvShowImage("FG", bg_model->foreground);
-		//imshow("foreground", bg_model->foreground);
-*/
-		
-		Mat tmp_bg_fg = depthf < 255; //(bg_model->foreground) & (depthf < 255); //not using bg-fg anymore
+				
+		Mat tmp_bg_fg = depthf < 255;	//anything not white is "real" depth
 		vector<Point> ctr;
 		Scalar blb = refineSegments(Mat(),tmp_bg_fg,out,ctr,midBlob); //find contours in the foreground, choose biggest
 
-//		imshow("rgb", rgbAndDepth);
-
 		if(blb[0]>=0 && blb[3] > 500) {
 			cvtColor(depthf, outC, CV_GRAY2BGR);
-//			blur(outC, outC, Size(25,25));
-//			vector<Mat> chns; split(outC, chns);
-//			chns[2] = chns[2] & out;
-//			merge(chns,outC);
 
 			//draw contour
 			Scalar color(0,0,255);
@@ -410,12 +313,9 @@ int main(int argc, char **argv)
 			}
 
 			if(registered) {
-//				stringstream ss; ss << "\"x\":" << (int)floor(lastMove.x - blb[0]) << ",\"y\":" << (int)floor(lastMove.y - blb[1]);
 				stringstream ss; ss << "\"x\":" << (int)floor(blb[0]*100.0/640.0) << ",\"y\":"<<(int)floor(blb[1]*100.0/480.0);
 				cout << "move: " << ss.str() << endl;
 				send_event("Move", ss.str());
-//				lastMove.x = blb[0]; lastMove.y = blb[1];
-				
 				
 				//---------------------- fist detection ---------------------
 				//calc laplacian of curve
@@ -436,16 +336,15 @@ int main(int argc, char **argv)
 					}
 				}
 				
-				
 				hc_stack.at(hc_stack_ptr) = hcr_ctr;
 				hc_stack_ptr = (hc_stack_ptr + 1) % hc_stack.size();
 				
 				Scalar _avg = mean(Mat(hc_stack));
-				if ((_avg[0] - (double)hcr_ctr) > 5.0) { //a big drop in curvature
+				if ((_avg[0] - (double)hcr_ctr) > 5.0) { //a big drop in curvature = hand fisted?
 					cout << "Hand click!" << endl;
 					send_event("HandClick", "");
 				}
-//				{
+//				{	//some debug on screen..
 //					stringstream ss; ss << "high curve pts " << hcr_ctr << ", avg " << _avg[0];
 //					putText(outC, ss.str(), Point(50,50), CV_FONT_HERSHEY_PLAIN, 2.0,Scalar(0,0,255), 2);
 //				}				
@@ -510,70 +409,6 @@ int main(int argc, char **argv)
 			send_event("Unregister", "");
 		}
 
-		/*
-		if (fr%5 == 0) {	//every 5 frames add more points to track
-			makePointsFromMask(out, prevPts,(fr%25 != 0));//clear points every 25 frame
-		}
-
-		cvtColor(frameMat, nextImg, CV_BGR2GRAY);
-//		imshow("prev", prevImg);
-//		imshow("next", nextImg);
-
-		calcOpticalFlowPyrLK(prevImg, nextImg, prevPts, nextPts, statusv, errv);
-		nextImg.copyTo(prevImg);
-
-		Mat ptsM(prevPts),nptsM(nextPts);
-		Mat statusM(statusv);
-		Scalar means = mean(ptsM-nptsM,statusM);
-
-		Scalar depthChange = mean(depthf-prevDepth,out);
-		imshow("depth change",depthf & (depthf < 254) & out);
-		imshow("just depth",depthf);
-		depthf.copyTo(prevDepth);
-
-//		cout << "average motion of largest blob: " <<
-//					means[0] << "," <<
-//					means[1] << "," <<
-//					depthChange[0] <<
-//					endl;
-
-		{
-			Mat _tmp; frameMat.copyTo(_tmp); //,out);
-			Point mid = Point(_tmp.cols/2, _tmp.rows/2);
-			line(_tmp, mid, mid+Point(means[0],0), Scalar(255,0,0), 5);
-			line(_tmp, mid, mid+Point(0,means[1]), Scalar(0,255,0), 5);
-			drawPoint(_tmp,prevPts,Scalar(0,0,255)); //,Mat::ones(1, statusv.size(), CV_8UC1));
-			drawPoint(_tmp,nextPts,Scalar(255,0,0),&statusM);
-			if(fabs(means[0])>2 && fabs(means[0]) < 60) {
-				cursor.x -= means[0];
-				//				stringstream ss; ss << "Move right-left";
-				//				putText(_tmp, ss.str(), Point(30,30), CV_FONT_HERSHEY_PLAIN, 2.0, Scalar(255,0,255), 2);
-			} else if(fabs(means[1])>2 && fabs(means[1]) < 60) {
-				cursor.y -= means[1];
-				//				stringstream ss; ss << "Move up-down";
-				//				putText(_tmp, ss.str(), Point(50,50), CV_FONT_HERSHEY_PLAIN, 2.0, Scalar(255,255,0), 2);
-			} else if (depthChange[0] > 50) {
-				cursor.width += depthChange[0]/10;
-				cursor.height += depthChange[0]/10;
-			} else {
-				nmfr++;
-			}
-
-			rectangle(_tmp, cursor, Scalar(0,0,255), 2);
-			imshow("out", _tmp);
-		}
-		prevPts = nextPts;
-		 */
-
-//		if (nmfr%15 == 0) {
-//			cursor.x = frameMat.cols/2;
-//			cursor.y = frameMat.rows/2;
-//			cursor.width = cursor.height = 10;
-//			nmfr = 0;
-//		}
-		
-		
-
         char k = cvWaitKey(5);
         if( k == 27 ) break;
         if( k == ' ' )
@@ -585,9 +420,6 @@ int main(int argc, char **argv)
 	}
 
 	printf("-- done!\n");
-
-	//destroyWindow("rgb");
-	//destroyWindow("depth");
 
 	pthread_join(ocv_thread, NULL);
 	pthread_exit(NULL);
