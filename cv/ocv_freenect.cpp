297219acd01f366de6dd8a41f3f5c42a7871d1c1
Doug Fritz <doug@media.mit.edu>
2010-12-06 21:50:27 -0500

    updated to work with the latest libfreenect

--- cv/ocv_freenect.cpp
+++ cv/ocv_freenect.cpp
@@ -53,28 +53,30 @@ void depth_cb(freenect_device *dev, void *depth, uint32_t timestamp)
 	pthread_mutex_lock(&buf_mutex);
 
 	//copy to ocv buf...
-	memcpy(depthMat.data, depth, FREENECT_DEPTH_SIZE);
+	//memcpy(depthMat.data, depth, FREENECT_DEPTH_SIZE);
+	memcpy(depthMat.data, depth, FREENECT_DEPTH_11BIT_SIZE);
 
 	got_frames++;
 	pthread_cond_signal(&frame_cond);
 	pthread_mutex_unlock(&buf_mutex);
 }
 
-void rgb_cb(freenect_device *dev, freenect_pixel *rgb, uint32_t timestamp)
+void rgb_cb(freenect_device *dev, void *rgb, uint32_t timestamp)
 {
 	pthread_mutex_lock(&buf_mutex);
 	got_frames++;
 	//copy to ocv_buf..
-	memcpy(rgbMat.data, rgb, FREENECT_RGB_SIZE);
+	//memcpy(rgbMat.data, rgb, FREENECT_RGB_SIZE);
+	memcpy(rgbMat.data, rgb, FREENECT_VIDEO_RGB_SIZE);
 
 	pthread_cond_signal(&frame_cond);
 	pthread_mutex_unlock(&buf_mutex);
 }
 
 //functions defined in the other file...
-extern Scalar refineSegments(const Mat& img, 
-							 Mat& mask, 
-							 Mat& dst, 
+extern Scalar refineSegments(const Mat& img,
+							 Mat& mask,
+							 Mat& dst,
 							 vector<Point>& contour,
 							 vector<Point>& second_contour,
 							 Point2i& previous);
@@ -95,40 +97,42 @@ void send_event(const string& etype, const string& edata) {
 
 int initFreenect() {
 //	int res = 0;
-	
+
 	//setup Freenect...
 	if (freenect_init(&f_ctx, NULL) < 0) {
 		printf("freenect_init() failed\n");
 		return 1;
 	}
-	
+
 	freenect_set_log_level(f_ctx, FREENECT_LOG_ERROR);
-	
+
 	int nr_devices = freenect_num_devices (f_ctx);
 	printf ("Number of devices found: %d\n", nr_devices);
-	
+
 	int user_device_number = 0;
 //	if (argc > 1)
 //		user_device_number = atoi(argv[1]);
-//	
+//
 //	if (nr_devices < 1)
 //		return 1;
-	
+
 	if (freenect_open_device(f_ctx, &f_dev, user_device_number) < 0) {
 		printf("Could not open device\n");
 		return 1;
 	}
-	
+
 	freenect_set_tilt_degs(f_dev,freenect_angle);
 	freenect_set_led(f_dev,LED_RED);
 	freenect_set_depth_callback(f_dev, depth_cb);
-	freenect_set_rgb_callback(f_dev, rgb_cb);
-	freenect_set_rgb_format(f_dev, FREENECT_FORMAT_RGB);
-	freenect_set_depth_format(f_dev, FREENECT_FORMAT_11_BIT);
-	
+	//freenect_set_rgb_callback(f_dev, rgb_cb);
+	//freenect_set_rgb_format(f_dev, FREENECT_FORMAT_RGB);
+  freenect_set_video_callback(f_dev, rgb_cb);
+  freenect_set_video_format(f_dev, FREENECT_VIDEO_RGB);
+	freenect_set_depth_format(f_dev, FREENECT_DEPTH_11BIT);
+
 	freenect_start_depth(f_dev);
-	freenect_start_rgb(f_dev);
-	
+	freenect_start_video(f_dev);
+
 	//start the freenect thread to poll for events
 //	res = pthread_create(&ocv_thread, NULL, freenect_threadfunc, NULL);
 //	if (res) {
@@ -136,11 +140,11 @@ int initFreenect() {
 //		return 1;
 //	}
 	return 0;
-}	
+}
 
 void send_image(const Mat& img) {
 //	s_sendmore(socket, "image");
-//	
+//
 //	Mat _img;
 //	if(img.type() == CV_8UC1) {
 //		Mat _tmp; resize(img, _tmp,Size(160,120)); //better to resize the gray data and not RGB
@@ -148,7 +152,7 @@ void send_image(const Mat& img) {
 //	} else {
 //		resize(img, _img, Size(160,120));
 //	}
-//	
+//
 //	s_send(socket, (const char*)_img.data);
 }
 
@@ -156,56 +160,56 @@ void send_image(const Mat& img) {
 Mat laplacian_mtx(int N, bool closed_poly) {
 	Mat A = Mat::zeros(N, N, CV_64FC1);
 	Mat d = Mat::zeros(N, 1, CV_64FC1);
-    
+
     //## endpoints
 	//if(closed_poly) {
 	A.at<double>(0,1) = 1;
 	d.at<double>(0,0) = 1;
-	
+
 	A.at<double>(N-1,N-2) = 1;
 	d.at<double>(N-1,0) = 1;
 	//} else {
 	//      A.at<double>(0,1) = 1;
 	//      d.at<double>(0,0) = 1;
 	//}
-    
+
     //## interior points
 	for(int i = 1; i <= N-2; i++) {
         A.at<double>(i, i-1) = 1;
         A.at<double>(i, i+1) = 1;
-        
+
         d.at<double>(i,0) = 0.5;
 	}
-    
+
 	Mat Dinv = Mat::diag( d );
-    
+
 	return Mat::eye(N,N,CV_64FC1) - Dinv * A;
 }
 
 void calc_laplacian(Mat& X, Mat& Xlap) {
 	static Mat lapX = laplacian_mtx(X.rows,false);
 	//a feeble attempt to save up in memory allocation.. in 99.9% of the cases this if fires
-	if(lapX.rows != X.rows) lapX = laplacian_mtx(X.rows,false); 
-	
+	if(lapX.rows != X.rows) lapX = laplacian_mtx(X.rows,false);
+
 	Mat _X;	//handle non-64UC2 matrices
 	if (X.type() != CV_64FC2) {
 		X.convertTo(_X, CV_64FC2);
 	} else {
 		_X = X;
 	}
-	
+
 	vector<Mat> v; split(_X,v);
 	v[0] = v[0].t() * lapX.t();
 	v[1] = v[1].t() * lapX.t();
 	cv::merge(v,Xlap);
-	
+
 	Xlap = Xlap.t();
 }
 
 
 void doHist(Mat& depthf, Mat& dmask) {
 //	cvtColor(src, hsv, CV_BGR2HSV);
-	
+
     // let's quantize the hue to 30 levels
     // and the saturation to 32 levels
     int hbins = 254;//, sbins = 32;
@@ -219,24 +223,24 @@ void doHist(Mat& depthf, Mat& dmask) {
     MatND hist;
     // we compute the histogram from the 0-th and 1-st channels
     int channels[] = {0};
-	
+
     double maxVal=0, minVal =0;
 	minMaxLoc(depthf, &minVal, 0, 0, 0, dmask);
-	
+
 	Mat depthtmp;
 	depthf.copyTo(depthtmp,(depthf - minVal) < 150);
-	
-	
-	
+
+
+
     calcHist( &depthtmp, 1, channels, dmask, // do not use mask
 			 hist, 1, histSize, ranges,
 			 true, // the histogram is uniform
 			 false );
     minMaxLoc(hist, 0, &maxVal, 0, 0);
-	
+
     int scale = 2;
     Mat histImg = Mat::zeros(hbins*scale, 100, CV_8UC3);
-	
+
 //	int s=0;
     for( int h = 0; h < hbins; h++ )
 //        for( int s = 0; s < sbins; s++ )
@@ -248,10 +252,10 @@ void doHist(Mat& depthf, Mat& dmask) {
 						Scalar::all(255),
 						CV_FILLED );
         }
-	
+
 //    namedWindow( "Source", 1 );
 //    imshow( "Source", src );
-	
+
 //    namedWindow( "H-S Histogram", 1 );
     imshow( "H-S Histogram", histImg );
 }
@@ -318,12 +322,12 @@ int main(int argc, char **argv)
 	freenect_set_tilt_degs(f_dev,freenect_angle);
 	freenect_set_led(f_dev,LED_RED);
 	freenect_set_depth_callback(f_dev, depth_cb);
-	freenect_set_rgb_callback(f_dev, rgb_cb);
-	freenect_set_rgb_format(f_dev, FREENECT_FORMAT_RGB);
-	freenect_set_depth_format(f_dev, FREENECT_FORMAT_11_BIT);
+	freenect_set_video_callback(f_dev, rgb_cb);
+	freenect_set_video_format(f_dev, FREENECT_VIDEO_RGB);
+	freenect_set_depth_format(f_dev, FREENECT_DEPTH_11BIT);
 
 	freenect_start_depth(f_dev);
-	freenect_start_rgb(f_dev);
+	freenect_start_video(f_dev);
 
 	//start the freenect thread to poll for events
 	res = pthread_create(&ocv_thread, NULL, freenect_threadfunc, NULL);
@@ -353,10 +357,10 @@ int main(int argc, char **argv)
 
 	Point2i midBlob(-1,-1);
 	Point2i lastMove(-1,-1);
-	
+
 	int hcr_ctr = -1;
 	vector<int> hc_stack(20); int hc_stack_ptr = 0;
-	
+
 	while (!die) {
 		fr++;
 
@@ -367,12 +371,12 @@ int main(int argc, char **argv)
 			_tmp.setTo(Scalar(2048), depthMat > 600.0);		//cut off at 600 to create a "box" where the user interacts
 			_tmp.convertTo(depthf, CV_8UC1, 255.0/1648.0);	//values are 0-2048 (11bit), account for -400 = 1648
 		}
-		
+
 //		{ //saving the frames to files for debug
 //			stringstream ss; ss << "depth_"<<fr<<".png";
 //			imwrite(ss.str(), depthf);
 //		}
-		
+
 		//Logarithm interpolation - try it!, It should be more "sensitive" for closer depths
 //		{
 //			Mat tmp,tmp1;
@@ -382,17 +386,17 @@ int main(int argc, char **argv)
 //		}
 //		imshow("depth",depthf);
 
-				
+
 		Mat tmp_bg_fg = depthf < 255;	//anything not white is "real" depth
 		vector<Point> ctr,ctr2;
-		
-		
+
+
 		Scalar blb = refineSegments(Mat(),tmp_bg_fg,out,ctr,ctr2,midBlob); //find contours in the foreground, choose biggest
 		/////// blb :
 		//blb[0] = x, blb[1] = y, blb[2] = 1st blob size, blb[3] = 2nd blob size.
-		
+
 //		uint mode_counters[3] = {0};
-		
+
 		if(blb[0]>=0 && blb[2] > 500) { //1st blob detected, and is big enough
 			cvtColor(depthf, outC, CV_GRAY2BGR);
 
@@ -401,7 +405,7 @@ int main(int argc, char **argv)
 			for (int idx=0; idx<ctr.size()-1; idx++)
 				line(outC, ctr[idx], ctr[idx+1], color, 1);
 			line(outC, ctr[ctr.size()-1], ctr[0], color, 1);
-			
+
 			if(ctr2.size() > 0) {
 				Scalar color2(255,0,255);
 				for (int idx=0; idx<ctr2.size()-1; idx++)
@@ -416,29 +420,29 @@ int main(int argc, char **argv)
 			line(outC, Point(blb[0]-_line[0]*70,blb[1]-_line[1]*70),
 						Point(blb[0]+_line[0]*70,blb[1]+_line[1]*70),
 						Scalar(255,255,0), 1);
-						
+
 			//blob center
 			circle(outC, Point(blb[0],blb[1]), 50, Scalar(255,0,0), 3);
-			
+
 			//closest point to the camera
 			Point minLoc; double minval;
 			minMaxLoc(depthMat, &minval, NULL, &minLoc, NULL, out);
 			circle(outC, minLoc, 5, Scalar(0,255,0), 3);
 			vector<int> c;
-			
+
 //			cout << "min depth " << minval << endl;
 
 			register_ctr = MIN((register_ctr + 1),60);
-			
+
 			if(blb[3] > 500)
 				register_secondbloc_ctr = MIN((register_secondbloc_ctr + 1),60);
-			
+
 			if (register_ctr > 30 && !registered) {
-				registered = true;   
+				registered = true;
 				appear.x = -1;
-				update_bg_model = false;				
-				lastMove.x = blb[0]; lastMove.y = blb[1];					
-				
+				update_bg_model = false;
+				lastMove.x = blb[0]; lastMove.y = blb[1];
+
 				if(register_secondbloc_ctr < 30) {
 					cout << "register panner" << endl;
 					send_event("Register", "\"mode\":\"panner\"");
@@ -452,16 +456,16 @@ int main(int argc, char **argv)
 				stringstream ss; ss << "\"x\":" << (int)floor(blb[0]*100.0/640.0) << ",\"y\":"<<(int)floor(blb[1]*100.0/480.0);
 				cout << "move: " << ss.str() << endl;
 				send_event("Move", ss.str());
-				
+
 				//---------------------- fist detection ---------------------
 				//calc laplacian of curve
 				vector<Point> approxCurve;	//approximate curve
 				approxPolyDP(curve, approxCurve, 10.0, true);
 				Mat approxCurveM(approxCurve);
-				
+
 				Mat curve_lap;
 				calc_laplacian(approxCurveM, curve_lap);	//calc laplacian
-				
+
 				hcr_ctr = 0;
 				for (int i=0; i<approxCurve.size(); i++) {
 					double n = norm(((Point2d*)(curve_lap.data))[i]);
@@ -471,27 +475,27 @@ int main(int argc, char **argv)
 						hcr_ctr++;
 					}
 				}
-				
+
 				hc_stack.at(hc_stack_ptr) = hcr_ctr;
 				hc_stack_ptr = (hc_stack_ptr + 1) % hc_stack.size();
-				
+
 				Scalar _avg = mean(Mat(hc_stack));
 				if ((_avg[0] - (double)hcr_ctr) > 5.0) { //a big drop in curvature = hand fisted?
 					cout << "Hand click!" << endl;
 					send_event("HandClick", "");
 				}
-				
+
 				if (mode_state == MODE_NONE) {
-					
+
 				}
-				
+
 //				imshow("out",out);
 				doHist(depthf,out);
-				
+
 				{	//some debug on screen..
 					stringstream ss; ss << "high curve pts " << hcr_ctr << ", avg " << _avg[0];
 					putText(outC, ss.str(), Point(50,50), CV_FONT_HERSHEY_PLAIN, 2.0,Scalar(0,0,255), 2);
-				}				
+				}
 			} else {
 				//not registered, look for gestures
 				if(appear.x<0) {
