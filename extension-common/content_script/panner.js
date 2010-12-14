86b27072236eef6208dddb9f2fa94348856d6b63
Aaron Zinman <aaron@azinman.com>
2010-12-13 23:44:20 -0500

    rest of saf rebuilding, start of correcting panner to be relative

--- extension-common/content_script/panner.js
+++ extension-common/content_script/panner.js
@@ -3,6 +3,7 @@ console.log("DepthJS: Loading Panner");
 
 DepthJS.panner.initTransform = null;
 DepthJS.panner.initTransition = null;
+DepthJS.panner.firstMove = null;
 
 DepthJS.panner.show = function() {
   if (DepthJS.panner.initTransform == null) {
@@ -15,6 +16,7 @@ DepthJS.panner.show = function() {
   $("body").css({"-webkit-transform":"scale(1.55) translate(0px,0px)",
                  "-webkit-transition-duration":"1s",
                  "-webkit-transform-origin":"50% " + centerPoint + "px"});
+  DepthJS.panner.firstMove = null;
 };
 
 DepthJS.panner.hide = function() {
@@ -25,12 +27,18 @@ DepthJS.panner.hide = function() {
     $("body").css({"-webkit-transform": DepthJS.panner.initTransform,
                    "-webkit-transition-duration": DepthJS.panner.initTransition});
   }, 1000);
+  DepthJS.panner.firstMove = null;
 };
 
 DepthJS.panner.move = function(x, y) {
+  if (DepthJS.panner.firstMove == null) {
+    DepthJS.panner.firstMove = [x, y];
+    return;
+  }
   var centerPoint = $(window).height()/2 + $(window).scrollTop();
+  // use firstMove here
   x = (x-50) * $(document).width() / 100;
-  y = -(y-50) * $(document).height() / 100;
+  y = (y-50) * $(document).height() / 100;
   $("body").css({"-webkit-transform":"scale(1.55) translate(" + x + "px, " + y + "px)",
                  "-webkit-transition-duration":".25s",
                  "-webkit-transform-origin":"50% " + centerPoint + "px"});
