a22423f4bfb7df401403a5d3f1acd088f3db7d93
Aaron Zinman <aaron@azinman.com>
2011-02-18 12:57:17 -0500

    Start on the cocoa webkit-plugin

---
+++ webkit-plugin-mac/webkit_plugin_macView.h
@@ -0,0 +1,22 @@
+//
+//  webkit_plugin_macView.h
+//  webkit-plugin-mac
+//
+//  Created by Aaron Zinman on 2/18/11.
+//  Copyright MIT Media Lab 2011. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface webkit_plugin_macView : NSView <WebPlugInViewFactory>
+{
+  BOOL haveInitDevice;
+  NSDictionary *pluginArguments;
+}
+- (NSDictionary *)pluginArguments;
+- (void)setPluginArguments:(NSDictionary *)value;
+  
+- (void) initDepthJS;
+- (void) shutdownDepthJS;
+          
+@end
