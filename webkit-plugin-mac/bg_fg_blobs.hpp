cc8d956109de80204c064b017abbd8a9eb33b742
Aaron Zinman <aaron@azinman.com>
2011-02-18 19:55:47 -0500

    got cocoa webkit plugin to at least load

---
+++ webkit-plugin-mac/bg_fg_blobs.hpp
@@ -0,0 +1,18 @@
+/*
+ *  bg_fg_blobs.h
+ *  OpenCVTries1
+ *
+ *  Created by Roy Shilkrot on 11/21/10.
+ *  Copyright 2010 MIT. All rights reserved.
+ *
+ */
+
+#include <vector>
+
+using namespace std;
+
+#include <cv.h>
+#include <highgui.h>
+#include <cvaux.h>
+
+using namespace cv;
