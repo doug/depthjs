8ba62c92f2d54f8e7bbc8e4ce5dd42a459a0b880
Roy Shilkrot <roys@media.mit.edu>
2011-03-06 01:38:37 -0500

    new gesture engine, still figuring stuff out, initial push

---
+++ webkit-plugin-mac/gesture_engine.hpp
@@ -0,0 +1,17 @@
+/*
+ *  gesture_engine.hpp
+ *  webkit-plugin-mac
+ *
+ *  Created by Roy Shilkrot on 3/6/11.
+ *  Copyright 2011 MIT. All rights reserved.
+ *
+ */
+
+#ifndef _GESTURE_ENGINE_HPP
+#define _GESTURE_ENGINE_HPP
+
+
+int gesture_engine(void* _arg);
+
+
+#endif
