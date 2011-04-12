b358631ceac15e561c6e663e15c7de4e032ad0de
Aaron Zinman <aaron@azinman.com>
2011-04-12 14:37:18 -0400

    less verbose

--- safari-extension-mac/DepthJS.safariextension/createHardLinks.sh
+++ safari-extension-mac/DepthJS.safariextension/createHardLinks.sh
@@ -6,7 +6,6 @@ mkdir extension-common
 find ../../extension-common -print0 | while read -d $'\0' file
 do
   curDir=`dirname ${file:6}`
-  echo "mkdir -p $curDir"
   mkdir -p $curDir
   ln $file ${file:6}
 done
