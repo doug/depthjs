d462c02ec2c3e2b45bbe67c896a0b17c230ec2ce
Aaron Zinman <aaron@azinman.com>
2011-04-12 20:01:55 -0400

    working on devleoper api...

--- extension-common/content_script/event_link.js
+++ extension-common/content_script/event_link.js
@@ -4,23 +4,32 @@ console.log("DepthJS: Loading Event Link");
 
 DepthJS.eventLink.initPort = function() {
   if (DepthJS.verbose) console.log("DepthJS: Event link init");
-  DepthJS.eventLink.domPort = $("<div id='DepthJS_eventPort' style='display:none'></div>");
+  DepthJS.eventLink.domPort = null;
   $(function() {
-    DepthJS.eventLink.domPort.appendTo("body");
+    var $eventPort = $("#DepthJS_eventPort")
+    if ($eventPort.length > 0 ) {
+      console.log("DepthJS: Registering eventPort on DepthJS supported web page");
+      DepthJS.eventLink.domPort = $eventPort;
+    }
+    //DepthJS.eventLink.domPort = $("<div id='DepthJS_eventPort' style='display:none'></div>");
+    //DepthJS.eventLink.domPort.appendTo("body");
   });
   DepthJS.browser.addContentScriptListener("event", DepthJS.eventLink.onEvent);
 };
 
 DepthJS.eventLink.onEvent = function (msg) {
   DepthJS.logSortaVerbose(msg.type, "DepthJS: event " + msg.type);
-  var handler = DepthJS.eventHandlers["on"+msg.type];
-  if (handler != null) {
-    handler(msg.data);
+  if (DepthJS.eventLink.domPort != null) {
+    // jQuery's trigger doesn't seem to work here for some reason
+    var event = document.createEvent("Event");
+    event.initEvent(msg.type, false, false);
+    DepthJS.eventLink.domPort.text(msg.jsonRep);
+    DepthJS.eventLink.domPort.get(0).dispatchEvent(event);
+  } else {
+    var handler = DepthJS.eventHandlers["on"+msg.type];
+    if (handler != null) {
+      handler(msg.data);
+    }
   }
-  // jQuery's trigger doesn't seem to work here for some reason
-  var event = document.createEvent("Event");
-  event.initEvent(msg.type, false, false);
-  DepthJS.eventLink.domPort.text(msg.jsonRep);
-  DepthJS.eventLink.domPort.get(0).dispatchEvent(event);
 };
 }
