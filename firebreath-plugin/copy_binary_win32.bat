553dfe6a4c121013eb613727a24afaffa5b65369
royshil <roy.shil@gmail.com>
2012-03-05 20:21:59 -0500

    fixing post build script

--- firebreath-plugin/copy_binary_win32.bat
+++ firebreath-plugin/copy_binary_win32.bat
@@ -1,5 +1,6 @@
-del "..\chrome-extension\plugin"
-rmdir "..\chrome-extension\plugin"
+echo off
+del /F /Q "..\chrome-extension\plugin"
+rmdir /S /Q "..\chrome-extension\plugin"
 mkdir "..\chrome-extension\plugin"
 copy "build\bin\depthjsplugin\Debug\npdepthjsplugin.dll" "..\chrome-extension\plugin"
-Debug\simple_templater.exe ..\chrome-extension\manifest.json.template $PLUGIN_PATH "{ "path": "plugin/npdepthjsplugin.dll" }" ..\chrome-extension\manifest.json
+Debug\simple_templater.exe ..\chrome-extension\manifest.json.template $PLUGIN_PATH "{ ""path"": ""plugin/npdepthjsplugin.dll"" }" ..\chrome-extension\manifest.json
